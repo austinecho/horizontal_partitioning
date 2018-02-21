00342d45da814d752d4cd6d0d40cfbff38fab5e3
Austin Pierce <Austin.Pierce@echo.com>
2018-02-21 17:07:33 -0600

    Commit EOD 2/21/2018

--- 1_EchoOptimizerImages_FileGroup_ParitionFunctionAndScheme.sql
+++ 1_EchoOptimizerImages_FileGroup_ParitionFunctionAndScheme.sql
@@ -1,6 +1,6 @@
 /*
 DataTeam
-Claims Partitioning
+EchoOptimizerImages Partitioning
 
 •	ADD New File Group
 •	ADD Partition Function
@@ -8,7 +8,7 @@ Claims Partitioning
 
 Run in DB01VPRD Equivilant 
 */
-USE Claims;
+USE EchoOptimizerImages;
 GO
 
 IF ( SELECT @@SERVERNAME ) = 'DB01VPRD' BEGIN PRINT 'Running in Environment DB01VPRD...'; END;
@@ -21,30 +21,30 @@ ELSE BEGIN PRINT 'ERROR: Server name not found. Process stopped.'; RETURN; END;
 --===================================================================================================
 PRINT '*** ADD FILE GROUP ***';
 
-IF NOT EXISTS ( SELECT 1 FROM sys.filegroups WHERE name = 'Claims_Archive' )
+IF NOT EXISTS ( SELECT 1 FROM sys.filegroups WHERE name = 'EchoOptimizerImages_Archive' )
 BEGIN 
-	ALTER DATABASE Claims ADD FILEGROUP Claims_Archive;
+	ALTER DATABASE EchoOptimizerImages ADD FILEGROUP EchoOptimizerImages_Archive;
 
 	IF ( SELECT @@SERVERNAME ) = 'DB01VPRD'
 	BEGIN
-		--PROD --Note: N:\Data\Claims.MDF --PRIMARY
-		ALTER DATABASE Claims ADD FILE ( NAME = 'Claims_Archive', FILENAME = N'N:\Data\Claims_Archive.NDF', SIZE = 1GB, MAXSIZE = UNLIMITED, FILEGROWTH = 500MB )
-		TO FILEGROUP Claims_Archive;
+		--PROD --Note: N:\Data\EchoOptimizerImages.MDF --PRIMARY
+		ALTER DATABASE EchoOptimizerImages ADD FILE ( NAME = 'EchoOptimizerImages_Archive', FILENAME = N'N:\Data\EchoOptimizerImages_Archive.NDF', SIZE = 1GB, MAXSIZE = UNLIMITED, FILEGROWTH = 500MB )
+		TO FILEGROUP EchoOptimizerImages_Archive;
 	END;
 	ELSE IF ( SELECT @@SERVERNAME ) = 'QA2-DB01'
 	BEGIN
-		--QA1 --Note: N:\Data\Claims.MDF --PRIMARY
-		ALTER DATABASE Claims ADD FILE ( NAME = 'Claims_Archive', FILENAME = N'N:\Data\Claims_Archive.NDF', SIZE = 1GB, MAXSIZE = UNLIMITED, FILEGROWTH = 500MB )
-		TO FILEGROUP Claims_Archive;
+		--QA1 --Note: N:\Data\EchoOptimizerImages.MDF --PRIMARY
+		ALTER DATABASE EchoOptimizerImages ADD FILE ( NAME = 'EchoOptimizerImages_Archive', FILENAME = N'N:\Data\EchoOptimizerImages_Archive.NDF', SIZE = 1GB, MAXSIZE = UNLIMITED, FILEGROWTH = 500MB )
+		TO FILEGROUP EchoOptimizerImages_Archive;
 	END;
 	ELSE IF ( SELECT @@SERVERNAME ) = 'DATATEAM4-DB01\DB01'
 	BEGIN
-		--DEV DT4 --Note: D:\Data\Claims\Claims_Primary.mdf --PRIMARY
-		ALTER DATABASE Claims ADD FILE ( NAME = 'Claims_Archive', FILENAME = N'D:\Data\Claims\Claims_Archive.NDF', SIZE = 1GB, MAXSIZE = UNLIMITED, FILEGROWTH = 500MB )
-		TO FILEGROUP Claims_Archive;
+		--DEV DT4 --Note: D:\Data\EchoOptimizerImages\EchoOptimizerImages_Primary.mdf --PRIMARY
+		ALTER DATABASE EchoOptimizerImages ADD FILE ( NAME = 'EchoOptimizerImages_Archive', FILENAME = N'D:\Data\EchoOptimizerImages\EchoOptimizerImages_Archive.NDF', SIZE = 1GB, MAXSIZE = UNLIMITED, FILEGROWTH = 500MB )
+		TO FILEGROUP EchoOptimizerImages_Archive;
 	END;
 
-	PRINT '- Filegroup [Claims_Archive] added';
+	PRINT '- Filegroup [EchoOptimizerImages_Archive] added';
 END;
 ELSE
 BEGIN
@@ -57,11 +57,11 @@ GO
 --===================================================================================================
 PRINT '*** ADD PARTITION FUNCTION ***';
 
-IF NOT EXISTS ( SELECT 1 FROM sys.partition_functions WHERE name = 'PF_Claims_DATETIME_3Year' )
+IF NOT EXISTS ( SELECT 1 FROM sys.partition_functions WHERE name = 'PF_EchoOptimizerImages_DATETIME_3Year' )
 BEGIN
-    CREATE PARTITION FUNCTION PF_Claims_DATETIME_3Year ( DATETIME ) AS RANGE RIGHT FOR VALUES ( '2015-01-01 00:00:00.000' ); --3YearsFromThisYear
+    CREATE PARTITION FUNCTION PF_EchoOptimizerImages_DATETIME_3Year ( DATETIME ) AS RANGE RIGHT FOR VALUES ( '2015-01-01 00:00:00.000' ); --3YearsFromThisYear
 
-    PRINT '- Partition Function [PF_Claims_DATETIME_3Year] added';
+    PRINT '- Partition Function [PF_EchoOptimizerImages_DATETIME_3Year] added';
 END;
 ELSE
 BEGIN
@@ -74,11 +74,11 @@ GO
 --===================================================================================================
 PRINT '*** ADD PARTITION SCHEME ***';
 
-IF NOT EXISTS ( SELECT 1 FROM sys.partition_schemes WHERE name = 'PS_Claims_DATETIME_3Year' )
+IF NOT EXISTS ( SELECT 1 FROM sys.partition_schemes WHERE name = 'PS_EchoOptimizerImages_DATETIME_3Year' )
 BEGIN
-    CREATE PARTITION SCHEME PS_Claims_DATETIME_3Year AS PARTITION PF_Claims_DATETIME_3Year TO ( Claims_Archive, [PRIMARY] );
+    CREATE PARTITION SCHEME PS_EchoOptimizerImages_DATETIME_3Year AS PARTITION PF_EchoOptimizerImages_DATETIME_3Year TO ( EchoOptimizerImages_Archive, [PRIMARY] );
 
-	PRINT '- Partition Scheme [PS_Claims_DATETIME_3Year] added';
+	PRINT '- Partition Scheme [PS_EchoOptimizerImages_DATETIME_3Year] added';
 END;
 ELSE
 BEGIN
@@ -88,17 +88,17 @@ GO
 
 --Verify: Check existance
 /*
-SELECT * FROM sys.partition_functions WHERE name = 'PF_Claims_DATETIME_3Year';
+SELECT * FROM sys.partition_functions WHERE name = 'PF_EchoOptimizerImages_DATETIME_3Year';
 
-SELECT * FROM sys.partition_schemes WHERE name = 'PS_Claims_DATETIME_3Year';
+SELECT * FROM sys.partition_schemes WHERE name = 'PS_EchoOptimizerImages_DATETIME_3Year';
 */
 
 /*
 Running in Environment QA2-DB01...
 *** ADD FILE GROUP ***
-- Filegroup [Claims_Archive] added
+- Filegroup [EchoOptimizerImages_Archive] added
 *** ADD PARTITION FUNCTION ***
-- Partition Function [PF_Claims_DATETIME_3Year] added
+- Partition Function [PF_EchoOptimizerImages_DATETIME_3Year] added
 *** ADD PARTITION SCHEME ***
-- Partition Scheme [PS_Claims_DATETIME_3Year] added
+- Partition Scheme [PS_EchoOptimizerImages_DATETIME_3Year] added
 */
--- 2_DB01_EchoOptimizerImages_Remove_From_Replication.sql
+++ 2_DB01_EchoOptimizerImages_Remove_From_Replication.sql
@@ -1,17 +1,17 @@
 /*
 DataTeam
-Claims Partitioning
+EchoOptimizerImages Partitioning
 
 Remove table from replication, will add back after schema changes
 
 Run in DB01VPRD Equivilant 
 */
-USE Claims
+USE EchoOptimizerImages
 GO
 
-EXEC sys.sp_dropsubscription @publication = 'PublicationClaims',@article ='dbo.AuditRecords', @subscriber = N'all', @destination_db = N'all'
-EXEC sp_droparticle @publication = 'PublicationClaims', @article  ='dbo.AuditRecords', @force_invalidate_snapshot = 1 
+EXEC sys.sp_dropsubscription @publication = 'PublicationEchoOptImages',@article ='DocumentManagementReport.Image', @subscriber = N'all', @destination_db = N'all'
+EXEC sp_droparticle @publication = 'PublicationEchoOptImages', @article  ='DocumentManagementReport.Image', @force_invalidate_snapshot = 1 
 
-EXEC sys.sp_dropsubscription @publication = 'PublicationClaims',@article ='dbo.AuditRecordFields', @subscriber = N'all', @destination_db = N'all'
-EXEC sp_droparticle @publication = 'PublicationClaims', @article  ='dbo.AuditRecordFields', @force_invalidate_snapshot = 1 
+EXEC sys.sp_dropsubscription @publication = 'PublicationEchoOptImages',@article ='dbo.FastLaneDocs', @subscriber = N'all', @destination_db = N'all'
+EXEC sp_droparticle @publication = 'PublicationEchoOptImages', @article  ='dbo.FastLaneDocs', @force_invalidate_snapshot = 1 
 
