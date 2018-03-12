307b37d22259ba1274931a55173ada10b936d53b
Austin Pierce <Austin.Pierce@echo.com>
2018-03-12 16:03:58 -0500

    Rep v2

--- 5_DB01_EchoOptimizerImages_Add_Back_To_Replication.sql
+++ 5_DB01_EchoOptimizerImages_Add_Back_To_Replication.sql
@@ -1,103 +1,61 @@
-/*
-DataTeam
-EchoOptimizerImages Partitioning
+/*----------------------------------------------------------------------------------
+ INSTRUCTIONS 
+------------------------------------------------------------------------------------
+1. Check EchoTrak replication is working (Replication Monitor tracer)
+2. Pause replication (Stop Synchronizing in Replication Monitor)
+3. Run "PART 1" of the script
+4. Start Snapshot Agent in Replication Monitor and wait until it completed
+5. Resume replication (Start Synchronizing in Replication Monitor)
+6. Wait until snapshot for 3 articles is delivered and check tracer
+7. Run "PART 2" of the script
+----------------------------------------------------------------------------------*/
 
-Add table back to replication
-
-Run in DB01VPRD Equivilant 
-*/
-USE EchoOptimizerImages
+-- PART 1
+USE [EchoTrak]
 GO
 
--- Pause Replication
-EXEC sys.sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'allow_anonymous', @value = 'false';
-EXEC sys.sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'immediate_sync', @value = 'false';
+DECLARE @subscriber sysname
 
+SET @subscriber = 
+	CASE @@SERVERNAME
+		WHEN 'QA1-DB01' THEN 'QA1-DB02.qa.echogl.net'
+		WHEN 'QA2-DB01' THEN 'QA2-DB02.qa.echogl.net'
+		WHEN 'QA3-DB01' THEN 'QA3-DB02.qa.echogl.net'
+		WHEN 'QA4-DB01' THEN 'QA4-DB02.qa.echogl.net'
+		WHEN 'DB01VPRD' THEN 'DB02VPRD'
+	END
+
+SELECT @subscriber as [Subscriber Server]
+
+IF @subscriber IS NULL
+BEGIN
+	PRINT 'ERROR: Wrong environment or SQL Server.';
+	RETURN;
+END;
+
+-- publication
+EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'allow_anonymous', @value = 'false'
+EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'immediate_sync', @value = 'false'
 -- Make sure the DDL changes are carried over
-EXEC sys.sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'replicate_ddl', @value = '1';
+EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'replicate_ddl', @value = '1'
+
+-- article
+EXEC sp_addarticle @publication = N'PublicationEchoTrak', @article = N'dbo.tblLoadCustomer', @source_owner = N'dbo', @source_object = N'tblLoadCustomer', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'truncate', @schema_option = 0x000000000803109F, @identityrangemanagementoption = N'manual', @destination_table = N'tblLoadCustomer', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'CALL [sp_MSins_dbotblLoadCustomer]', @del_cmd = N'CALL [sp_MSdel_dbotblLoadCustomer]', @upd_cmd = N'SCALL [sp_MSupd_dbotblLoadCustomer]', @force_invalidate_snapshot = 1
+EXEC sp_addsubscription @publication = N'PublicationEchoTrak', @subscriber = @subscriber, @destination_db = N'EchoTrak', @subscription_type = N'Push', @sync_type = N'automatic', @article = N'dbo.tblLoadCustomer', @subscriber_type = 0, @reserved='Internal'
+
+EXEC sp_addarticle @publication = N'PublicationEchoTrak', @article = N'dbo.tblLoadStop', @source_owner = N'dbo', @source_object = N'tblLoadStop', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'truncate', @schema_option = 0x000000000803109F, @identityrangemanagementoption = N'manual', @destination_table = N'tblLoadStop', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'CALL [sp_MSins_dbotblLoadStop]', @del_cmd = N'CALL [sp_MSdel_dbotblLoadStop]', @upd_cmd = N'SCALL [sp_MSupd_dbotblLoadStop]', @force_invalidate_snapshot = 1
+EXEC sp_addsubscription @publication = N'PublicationEchoTrak', @subscriber = @subscriber, @destination_db = N'EchoTrak', @subscription_type = N'Push', @sync_type = N'automatic', @article = N'dbo.tblLoadStop', @subscriber_type = 0, @reserved='Internal'
+
+
+
+
+/*
+-- PART 2
+-- publication
+EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'immediate_sync', @value = 'true'
+EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'allow_anonymous', @value = 'true'
+ --Make sure the DDL changes are not carried over
+EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'replicate_ddl', @value = '0'
+*/
+
 GO
-
-/*****   Run script to re-add affected article(s) back to replication.   ************/
-EXEC sys.sp_addarticle @publication = N'PublicationEchoOptImages'
-                     , @article = N'Image'
-                     , @source_owner = N'DocumentManagementReport'
-                     , @source_object = N'Image'
-                     , @type = N'logbased'
-                     , @description = N''
-                     , @creation_script = N''
-                     , @pre_creation_cmd = N'truncate'
-                     , @schema_option = 0x000000000803509F
-                     , @identityrangemanagementoption = N'manual'
-                     , @destination_table = N'Image'
-                     , @destination_owner = N'DocumentManagementReport'
-                     , @status = 24
-                     , @vertical_partition = N'false';
-
-EXEC sys.sp_addarticle @publication = N'PublicationEchoOptImages'
-                     , @article = N'FastLaneDocs'
-                     , @source_owner = N'dbo'
-                     , @source_object = N'FastLaneDocs'
-                     , @type = N'logbased'
-                     , @description = N''
-                     , @creation_script = N''
-                     , @pre_creation_cmd = N'truncate'
-                     , @schema_option = 0x000000000803509F
-                     , @identityrangemanagementoption = N'manual'
-                     , @destination_table = N'FastLaneDocs'
-                     , @destination_owner = N'dbo'
-                     , @status = 24
-                     , @vertical_partition = N'false';
-GO
-
-/*****   Run script to re-add affected subscriptions back to replication.   ************/
-
-EXEC sp_addSubscription @publication ='PublicationEchoOptImages',@subscriber = 'QA2-DB02.qa.echogl.net',@destination_db='EchoOptimizerImages',@reserved='Internal',@article='DocumentManagementReport.Image',@sync_type='automatic'
-EXEC sp_addSubscription @publication ='PublicationEchoOptImages',@subscriber = 'QA2-DB02.qa.echogl.net',@destination_db='EchoOptimizerImages',@reserved='Internal',@article='dbo.FastLaneDocs',@sync_type='automatic'
-GO
-
-
--- ===================================================================================================
-
--- ***** IMPORTANT MANUAL STEP (UNTIL WE FIGURE OUT HOW TO AUTOMATICALLY DO THIS) *****
--- Remote in to SQL Server Box 
--- Start SSMS
--- Connect to DB01
--- Expand Replication / Local Publications
--- Right Click the Publication, choose "View Snapshot Agent Status"
--- Click Start
--- You should see it say "Starting Agent"
--- Within a minute or so (depending on number of articles modified), you should see:
---      [100%] A snapshot of 1 article(s) was generated.
-
--- ===================================================================================================
---
--- If Server doesn't have SSMS, you can also start snapshot agent by running script below:
-EXECUTE sys.sp_startpublication_snapshot @publication = 'PublicationEchoOptImages';
---
--- Check Run Status of Snapshot:
--- If [runstatus] = 2 and [comments] starts with [100%] then snapshot job is done
-USE distribution;
-GO
-
-SELECT     TOP ( 20 ) a.runstatus, a.start_time, a.time, a.duration, a.comments
-FROM       distribution.dbo.MSsnapshot_history AS a
-INNER JOIN distribution.dbo.MSsnapshot_agents AS b
-        ON b.id = a.agent_id
-WHERE      b.publisher_db = 'EchoOptimizerImages'
-ORDER BY   a.time DESC;
-
--- Snapshot Done
--- ===================================================================================================
---
-
--- Re-Enable the publication
-USE EchoOptimizerImages
-GO
-
-EXEC sys.sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'immediate_sync', @value = 'true';
-EXEC sys.sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'allow_anonymous', @value = 'true';
-
--- Make sure the DDL changes are NOT carried over any more
-EXEC sys.sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'replicate_ddl', @value = '0';
-GO
-
