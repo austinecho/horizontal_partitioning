87266eb7b517b7cb7c12f6f7567c4e9370b6e57b
Austin Pierce <Austin.Pierce@echo.com>
2018-03-12 16:20:09 -0500

    Replication v2 update

--- 5_DB01_EchoOptimizerImages_Add_Back_To_Replication.sql
+++ 5_DB01_EchoOptimizerImages_Add_Back_To_Replication.sql
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------------
  INSTRUCTIONS 
 ------------------------------------------------------------------------------------
-1. Check EchoTrak replication is working (Replication Monitor tracer)
+1. Check EchoOptimizerImages replication is working (Replication Monitor tracer)
 2. Pause replication (Stop Synchronizing in Replication Monitor)
 3. Run "PART 1" of the script
 4. Start Snapshot Agent in Replication Monitor and wait until it completed
@@ -11,7 +11,7 @@
 ----------------------------------------------------------------------------------*/
 
 -- PART 1
-USE [EchoTrak]
+USE [EchoOptimizerImages]
 GO
 
 DECLARE @subscriber sysname
@@ -34,17 +34,21 @@ BEGIN
 END;
 
 -- publication
-EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'allow_anonymous', @value = 'false'
-EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'immediate_sync', @value = 'false'
+EXEC sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'allow_anonymous', @value = 'false'
+EXEC sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'immediate_sync', @value = 'false'
 -- Make sure the DDL changes are carried over
-EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'replicate_ddl', @value = '1'
+EXEC sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'replicate_ddl', @value = '1'
 
 -- article
-EXEC sp_addarticle @publication = N'PublicationEchoTrak', @article = N'dbo.tblLoadCustomer', @source_owner = N'dbo', @source_object = N'tblLoadCustomer', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'truncate', @schema_option = 0x000000000803109F, @identityrangemanagementoption = N'manual', @destination_table = N'tblLoadCustomer', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'CALL [sp_MSins_dbotblLoadCustomer]', @del_cmd = N'CALL [sp_MSdel_dbotblLoadCustomer]', @upd_cmd = N'SCALL [sp_MSupd_dbotblLoadCustomer]', @force_invalidate_snapshot = 1
-EXEC sp_addsubscription @publication = N'PublicationEchoTrak', @subscriber = @subscriber, @destination_db = N'EchoTrak', @subscription_type = N'Push', @sync_type = N'automatic', @article = N'dbo.tblLoadCustomer', @subscriber_type = 0, @reserved='Internal'
+EXEC sp_addarticle @publication = N'PublicationEchoOptImages', @article = N'DocumentManagementReport.Image', @source_owner = N'DocumentManagementReport', @source_object = N'Image', @type = N'logbased', @description = N'', 
+@creation_script = N'', @pre_creation_cmd = N'truncate', @schema_option = 0x000000000803109F, @identityrangemanagementoption = N'manual', @destination_table = N'DocumentManagementReport.Image', @destination_owner = N'DocumentManagementReport', 
+@status = 24, @vertical_partition = N'false', @ins_cmd = N'CALL [sp_MSins_DocumentManagementReportImage]', @del_cmd = N'CALL [sp_MSdel_DocumentManagementReportImage]', @upd_cmd = N'SCALL [sp_MSupd_DocumentManagementReportImage]', @force_invalidate_snapshot = 1
+EXEC sp_addsubscription @publication = N'PublicationEchoOptImages', @subscriber = @subscriber, @destination_db = N'EchoOptimizerImages', @subscription_type = N'Push', @sync_type = N'automatic', @article = N'DocumentManagementReport.Image', @subscriber_type = 0, @reserved='Internal'
 
-EXEC sp_addarticle @publication = N'PublicationEchoTrak', @article = N'dbo.tblLoadStop', @source_owner = N'dbo', @source_object = N'tblLoadStop', @type = N'logbased', @description = N'', @creation_script = N'', @pre_creation_cmd = N'truncate', @schema_option = 0x000000000803109F, @identityrangemanagementoption = N'manual', @destination_table = N'tblLoadStop', @destination_owner = N'dbo', @status = 24, @vertical_partition = N'false', @ins_cmd = N'CALL [sp_MSins_dbotblLoadStop]', @del_cmd = N'CALL [sp_MSdel_dbotblLoadStop]', @upd_cmd = N'SCALL [sp_MSupd_dbotblLoadStop]', @force_invalidate_snapshot = 1
-EXEC sp_addsubscription @publication = N'PublicationEchoTrak', @subscriber = @subscriber, @destination_db = N'EchoTrak', @subscription_type = N'Push', @sync_type = N'automatic', @article = N'dbo.tblLoadStop', @subscriber_type = 0, @reserved='Internal'
+EXEC sp_addarticle @publication = N'PublicationEchoOptImages', @article = N'FastLaneDocs', @source_owner = N'dbo', @source_object = N'FastLaneDocs', @type = N'logbased', @description = N'', @creation_script = N'', 
+@pre_creation_cmd = N'truncate', @schema_option = 0x000000000803109F, @identityrangemanagementoption = N'manual', @destination_table = N'FastLaneDocs', @destination_owner = N'dbo', @status = 24, 
+@vertical_partition = N'false', @ins_cmd = N'CALL [sp_MSins_dboFastLaneDocs]', @del_cmd = N'CALL [sp_MSdel_dboFastLaneDocs]', @upd_cmd = N'SCALL [sp_MSupd_dboFastLaneDocs]', @force_invalidate_snapshot = 1
+EXEC sp_addsubscription @publication = N'PublicationEchoOptImages', @subscriber = @subscriber, @destination_db = N'EchoOptimizerImages', @subscription_type = N'Push', @sync_type = N'automatic', @article = N'dbo.FastLaneDocs', @subscriber_type = 0, @reserved='Internal'
 
 
 
@@ -52,10 +56,10 @@ EXEC sp_addsubscription @publication = N'PublicationEchoTrak', @subscriber = @su
 /*
 -- PART 2
 -- publication
-EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'immediate_sync', @value = 'true'
-EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'allow_anonymous', @value = 'true'
+EXEC sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'immediate_sync', @value = 'true'
+EXEC sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'allow_anonymous', @value = 'true'
  --Make sure the DDL changes are not carried over
-EXEC sp_changepublication @publication = 'PublicationEchoTrak', @property = 'replicate_ddl', @value = '0'
+EXEC sp_changepublication @publication = 'PublicationEchoOptImages', @property = 'replicate_ddl', @value = '0'
 */
 
 GO
